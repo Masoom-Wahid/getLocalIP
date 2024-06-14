b4129ba4f256e345d4d192cbac6c76dc4c8163de
Masoom-Wahid <masoom3892@gmail.com>
2024-06-14 18:39:41 +0430

    changed to lua

---
+++ main.lua
@@ -0,0 +1,25 @@
+Window = {} -- create a namespace
+
+-- create the prototype with default values
+Window.prototype = {x=0, y=0, width=100, height=100}
+Window.mt = {} --create a metatable
+
+--declare the constructor function
+function Window.new( o )
+  setmetatable(o, Window.mt)
+	return o
+end
+
+Window.mt.__index = function ( table, key )
+	return Window.prototype[key]
+end
+
+w = Window.new{x=10, y=20}
+
+
+local res = fetch({
+    url = "https://api.ipify.org?format=json",
+})
+
+local main_div = get("maindiv")
+main_div.set_contents(res.ip)
