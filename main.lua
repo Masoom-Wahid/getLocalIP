66c4f35b5b921a5ccb2f06528027a8d1ba0552fd
Masoom-Wahid <masoom3892@gmail.com>
2024-06-14 18:47:41 +0430

    fix

--- main.lua
+++ main.lua
@@ -1,20 +1,20 @@
-Window = {} -- create a namespace
+-- Window = {} -- create a namespace
 
--- create the prototype with default values
-Window.prototype = {x=0, y=0, width=100, height=100}
-Window.mt = {} --create a metatable
+-- -- create the prototype with default values
+-- Window.prototype = {x=0, y=0, width=100, height=100}
+-- Window.mt = {} --create a metatable
 
---declare the constructor function
-function Window.new( o )
-  setmetatable(o, Window.mt)
-	return o
-end
+-- --declare the constructor function
+-- function Window.new( o )
+--   setmetatable(o, Window.mt)
+-- 	return o
+-- end
 
-Window.mt.__index = function ( table, key )
-	return Window.prototype[key]
-end
+-- Window.mt.__index = function ( table, key )
+-- 	return Window.prototype[key]
+-- end
 
-w = Window.new{x=10, y=20}
+-- w = Window.new{x=10, y=20}
 
 
 local res = fetch({
